85cd82b2fc2b8b4d4774d9276e8209ce2e753ee8
jasczajka <czajka.jan12@gmail.com>
2022-04-26 15:41:07 +0200

    Make number buttons add numbers to the display

--- myScript.js
+++ myScript.js
@@ -17,3 +17,11 @@ function divide(a, b){
 function operate(operator, a, b){
   return operator(a,b);
 }
+
+//add typing functionality to number buttons
+let numberButtons = document.querySelectorAll('.number-button');
+numberButtons.forEach(button => button.addEventListener('click', (e) => {
+  document.querySelector('#display').textContent = document.querySelector('#display').textContent + e.target.textContent;
+  //store the display value in a separate variable
+  let displayValue = document.querySelector('#display').textContent;
+}));
