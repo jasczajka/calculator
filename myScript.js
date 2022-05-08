40afdd5615c70ee4a8b0c0a23d6a570d9ad0e515
jasczajka <czajka.jan12@gmail.com>
2022-05-08 18:31:10 +0200

    Add working clear button functionality

--- myScript.js
+++ myScript.js
@@ -37,6 +37,9 @@ let operators = {
 
 operatorButtons.forEach(button => button.addEventListener('click', makeOperation));
 function makeOperation(e){
+
+  
+  
   //get the first number for operation
   let firstNumber = document.querySelector('#display').textContent;
   //empty display so the second number can be entered
@@ -65,26 +68,21 @@ function makeOperation(e){
     //remove the listeners so we can wait for another operation
     document.querySelector('#equals-button').removeEventListener('click',equalsFunction);
     operatorButtons.forEach(button => button.removeEventListener('click',equalsFunction));
-    
   };
-
   //add functionality of finishing an operation back
   document.querySelector('#equals-button').addEventListener('click', equalsFunction);
   operatorButtons.forEach(button => button.addEventListener('click', equalsFunction));
   //add functionality of beginning an operation back
   operatorButtons.forEach(button => button.addEventListener('click', makeOperation));
-
-  
+//if makeOperation already triggered and we want to use the clear button, we need to remove the equalsFunction operator
+document.querySelector('#clear-button').addEventListener('click', () =>{
+  document.querySelector('#equals-button').removeEventListener('click', equalsFunction);
+  operatorButtons.forEach(button => button.removeEventListener('click', equalsFunction));
+});  
 };
 
-//add clear button functionality
-// function clearCalculator(){
-//   document.querySelector('#display').textContent = '';
-//   document.querySelectorAll('button').forEach(button => button.style.backgroundColor = 'grey');
-//   //if an operation has begun, reset the functionality 
-//   document.querySelector('#equals-button').removeEventListener('click',makeOperation);
-//   operatorButtons.forEach(button => button.removeEventListener('click',makeOperation));
-//   document.querySelector('#equals-button').addEventListener('click',makeOperation);
-//   operatorButtons.forEach(button => button.addEventListener('click',makeOperation));
-// };
-// document.querySelector('#clear-button').addEventListener('click',clearCalculator);
+function clearCalculator(){
+  document.querySelector('#display').textContent = '';
+  document.querySelectorAll('button').forEach(button => button.style.backgroundColor = 'grey');
+};
+document.querySelector('#clear-button').addEventListener('click',clearCalculator);
